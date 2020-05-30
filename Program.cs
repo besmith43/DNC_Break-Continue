e091f4629d122e28f30ab21be1c7c21d3c47d14b
Blake Smith <besmith43@gmail.com>
2020-05-30 15:57:43 -0500

    completed

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DNC_Break_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            while (num < 5)
+            {
+                Console.WriteLine("Running contents of the while loop");
+                num++;
+
+                if (num == 3)
+                {
+                    Console.WriteLine("If statement is true, and now exiting the while loop");
+                    break;
+                }
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"{i} is an odd number");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
